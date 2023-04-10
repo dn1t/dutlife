324a2b853d72fd902bf839b62ce5821e7273f214
thoratica <me@tica.fun>
2023-04-10 22:59:33 +0900

    :tada: dut.life

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-9xl font-bold font-display">dut.life</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ['"Pretendard Variable"'],
+      display: ['"Onest"', '"Pretendard Variable"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
